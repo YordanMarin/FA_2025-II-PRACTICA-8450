3e289197a1fedcaaa5f8688b494b2413545aafe5
yordan <yordan@gmail.com>
2025-09-02 15:31:47 -0500

    subiendo ejercicio1-SEM1

---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,7 @@
+﻿def ejer1(): #creando metodo ejer1
+    nombre = input("Ingrese su nombreP: ")
+    carrera = input("Ingese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido a FA de {carrera}") 
+
+ejer1()
