e1ff129959caf1135d0b25cc94a877c294592091
yordan <yordan@gmail.com>
2025-09-16 15:55:18 -0500

    ejer2 sem5

--- SEMANA4-PY/SEMANA4_PY.py
+++ SEMANA4-PY/SEMANA4_PY.py
@@ -9,5 +9,58 @@ def ejer1():
         else:
             print("Adulto mayor")
 
-ejer1()
+def ejer2():
+    annio = int(input("Ingrese el año: "))
+
+    if (annio %4 ==0 and annio %100 != 0) or (annio %400 ==0):
+        print("\nEl año es bisiesto")
+    else:
+        print("\nEl año no es bisiesto")
+
+    if annio %2 ==0:
+        print("El año es par")
+    else:
+        print("El año es impar")
+
+def ejer3():
+    monto = float(input("Ingrese el monto en soles: "))
+
+    print("\n1. Dolares\n2. Euros")
+
+    opcion=int(input("\nIngrese una opción: "))
+
+    match opcion:
+        case 1: print("Dolares: ", round((monto/3.75),0))
+        case 2: print(f"Euros: {monto/4.05:.2f}")
+        case _: print("Opción incorrecta")
+
+import math
+
+def ejer4():
+    print("Bienvenido al sistema de calculos de áreas\n")
+    print("1. Cuadrado")
+    print("2. Rectangulo")
+    print("3. Triangulo")
+    print("4. Circulo\n")
+
+    opcion = int(input("Ingrese una opción: "))
+
+    match opcion:
+        case 1:
+            lado = int(input("Ingrese lado: "))
+            print("Área cuadrado: ",lado*lado)
+        case 2:
+            bse = int(input("Ingrese la base: "))
+            alt = int(input("Ingrese la altura: "))
+            print("Área rectangulo: ",(bse*alt))
+        case 3:
+            bse2 = int(input("Ingrese la base: "))
+            alt2 = int(input("Ingrese la altura: "))
+            print("Área triangulo: ",(bse2*alt2)/2)
+        case 4:
+            radio = float(input("Ingrese el radio: "))
+            print("Área del circulo: ",(round(math.pi * radio**2),2))
+        case _: print("Opción incorrecta")
+
+ejer4()
 
