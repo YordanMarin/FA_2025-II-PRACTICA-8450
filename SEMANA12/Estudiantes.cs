ddd45e97204128d0b3df9241cfe60a3f970cc20d
Yordan <yordan@gmail.com>
2025-11-04 16:10:15 -0500

    sem12

---
+++ SEMANA12/Estudiantes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA12
+{
+    internal class Estudiantes
+    {
+        byte[] edades = new byte[0];
+        int pos = 0;
+
+        public void registrar()
+        {
+            byte ed;
+
+            while (true)
+            {
+                Console.Write("\nIngrese edad: ");
+                if (byte.TryParse(Console.ReadLine(), out ed) & ed >= 14 && ed <= 120) break;
+                else Console.WriteLine("Ingrese una edad correcta!\n");
+            }
+
+            Array.Resize(ref edades, edades.Length+1);
+            edades[pos] = ed;
+            pos++;
+            Console.WriteLine("\nEdad registrado correctamente.");
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("\nPOS\tEDAD");
+            for(int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine(i + "\t" + edades[i]);
+            }
+        }
+
+        public void eliminar()
+        {
+            int indice = -1;
+            Console.Write("\nIngrese la edad a eliminar: ");
+            byte ed = byte.Parse(Console.ReadLine());
+
+            for(int i = 0;i < edades.Length; i++)
+            {
+                if (edades[i] == ed)
+                    indice = i;
+            }
+
+            if (indice != -1)
+            {
+                for(int j=indice; j<edades.Length-1;j++)
+                    edades[j] = edades[j+1];
+
+                Array.Resize(ref edades, edades.Length - 1);
+                pos--;
+                Console.WriteLine("\nEdad eliminado correctamente.");
+            }
+            else Console.WriteLine("\nNo se puede eliminar porque no existe.");
+        }
+
+        public void ordenar()
+        {
+            for(int i = 0;i < edades.Length-1; i++)
+            {
+                for(int j =0;j<edades.Length-1-i; j++)
+                {
+                    if (edades[j] < edades[j + 1])
+                    {
+                        byte temp = edades[j];
+                        edades[j] = edades[j + 1];
+                        edades[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nSe ordeno correctamente.");
+        }
+    }
+}
---
+++ SEMANA12/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conti;
+            Estudiantes e = new Estudiantes();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("BIENVENDIOS AL SISTEMA DE REGISTRO DE EDADES\n");
+                Console.WriteLine("Menú de opciones:\n");
+                Console.WriteLine("1. Registrar");
+                Console.WriteLine("2. Mostrar");
+                Console.WriteLine("3. Eliminar");
+                Console.WriteLine("4. Ordenar");
+                Console.WriteLine("0. Salir\n");
+
+                int opc;
+
+                while (true)
+                {
+                    Console.Write("Ingrese una opción: ");
+                    if (int.TryParse(Console.ReadLine(), out opc) & opc >= 0 && opc <= 4) break;
+                    else Console.WriteLine("Ingrese una opción correcta!\n");
+                }
+                switch (opc)
+                {
+                    case 0: return;
+                    case 1: e.registrar(); break;
+                    case 2: e.mostrar();  break;
+                    case 3: e.eliminar(); break;
+                    case 4: e.ordenar(); break;
+                }
+
+                while (true)
+                {
+                    Console.Write("\n¿Desea continuar? (S/N): ");
+                    conti = Console.ReadLine();
+                    if (conti == "s" || conti == "n") break;
+                    else Console.WriteLine("Ingrese solo 's' o 'n'!\n");
+                }
+                Console.WriteLine("\nGracias por utilizar el sistema.");
+            } while (conti == "s");
+        }
+    }
+}
